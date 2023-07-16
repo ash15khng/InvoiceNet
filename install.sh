da8944c0fb5b5fb199174ef830161f78366bac0d
ash15khng <ash15khng@gmail.com>
2023-07-16 21:15:11 +0800

    added imagemagick to dependencies

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 # install dependencies
-sudo apt install -y python3-pip python3-virtualenv python3-tk tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
-# pip install virtualenv
+sudo apt install -y python3-pip python3-virtualenv python3-tk tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev imagemagick
 
 # create virtual environment
 virtualenv env -p python3
