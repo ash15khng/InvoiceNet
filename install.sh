4ae7c6c950018e39ff11190b57123d51557af5fd
ash15khng <ash15khng@gmail.com>
2023-07-16 21:09:32 +0800

    testing

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # install dependencies
-sudo apt install -y python3-pip python3-venv python3-virtualenv python3-tk tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
+sudo apt install -y python3-pip python3-virtualenv python3-tk tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
 # pip install virtualenv
 
 # create virtual environment
