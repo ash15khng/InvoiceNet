308c3d9a2ceebaf0af344c29223b97c00cb3860a
ash15khng <ash15khng@gmail.com>
2023-07-16 20:59:24 +0800

    removed pip virtualenv

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install dependencies
 sudo apt install -y python3-pip python3-venv python3-virtualenv python3-tk tesseract-ocr poppler-utils libxext-dev libsm-dev libxrender-dev
-pip install virtualenv
+# pip install virtualenv
 
 # create virtual environment
 virtualenv env -p python3
